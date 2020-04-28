512776e000d88a2d50455b3f988c34e390b86e74
vxggangxv <vxggangxv@naver.com>
2020-04-28 11:12:49 +0900

    edit

--- nurifunding/src/assets/js/livemate/ui.js
+++ nurifunding/src/assets/js/livemate/ui.js
@@ -16,7 +16,7 @@ var formFn = {
     formTextFn: function () {
         $('input[type=text], input[type=password]').on('input', function () {
             // console.log($(this).val());
-            $(this).css('color', '#000');
+            $(this).addClass('active');
             $(this).siblings('[data-role=form-text-remove]').addClass('active');
         });
         $('[data-role=form-text-remove]').on('click', function () {
@@ -27,7 +27,22 @@ var formFn = {
             $(this).siblings('[data-role=form-text-remove]').removeClass('active');
         });
         $("input:text[numberOnly]").on("keyup", function () {
-            $(this).val($(this).val().replace(/[^0-9]/g, ""));
+            if ($(this).hasClass('commas')) {
+                $(this).val(addCommas($(this).val().replace(/[^0-9]/g, "")));
+            } else {
+                $(this).val($(this).val().replace(/[^0-9]/g, ""));
+            }
         });
     }
+}
+
+//3자리 단위마다 콤마 생성
+function addCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+//모든 콤마 제거
+function removeCommas(x) {
+    if (!x || x.length == 0) return "";
+    else return x.split(",").join("");
 }
